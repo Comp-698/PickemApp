8648a2994686686c75ba05e3143c9fc3661764fd
DynData <tchadwick@dyn.com>
2015-11-16 18:37:50 -0800

    a web app kernel in pickem.go, and skeleton templates from Jon Shallow.  Note the import of PickemLib.  with an updated pull from upstream master, a go build pickem.go, and go run pickem.go should deploy a web application at http://localhost:8080/pickem/ which can be iterated.

---
+++ pickem.go
@@ -0,0 +1,54 @@
+// Copyright 2015 Google Inc. All rights reserved.
+// Use of this source code is governed by the Apache 2.0
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"log"
+	"net/http"
+    "regexp"
+    "html/template"
+    "github.com/Comp-698/PickemLib"
+)
+
+type Pickem struct {
+    Picks map[string]int
+    Name string
+}
+
+var validPath = regexp.MustCompile("^/(pickem|other)/([a-zA-Z0-9]+)$")
+
+// var templates = template.Must( template.ParseFiles( "pickem.html" ) )
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Pickem) {
+    err := templates.ExecuteTemplate(w, tmpl, p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+func pickemHandler(w http.ResponseWriter, r *http.Request) {
+    renderTemplate(w, "pickem", &Pickem{Name: "pickem"})
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        m := validPath.FindStringSubmatch(r.URL.Path)
+        if m == nil {
+            http.NotFound(w, r)
+            return
+        }
+        fn(w, r, m[2])
+    }
+}
+
+func main() {
+	http.HandleFunc("/pickem/", pickemHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte( PickemLib.Data("paste") ) )
+}
